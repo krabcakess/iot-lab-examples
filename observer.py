9047e31cab213857e08b4d942f91c826e2085bcf
Muhammad Izz Nabeel <s3986060@student.rmit.edu.au>
2026-04-02 10:56:51 +1100

    lab 5 code

---
+++ observer.py
@@ -0,0 +1,39 @@
+# reference: https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_decorator.htm
+# adopted for learning pursposes only
+
+import threading
+import time
+import pdb
+
+class Downloader(threading.Thread):
+   
+   def run(self):
+      print 'downloading'
+      for i in range(1,5):
+         self.i = i
+         time.sleep(2)
+			print 'unfunf'
+         return 'hello world'
+
+class Worker(threading.Thread):
+   def run(self):
+      for i in range(1,5):
+         print 'worker running: %i (%i)' % (i, t.i)
+         time.sleep(1)
+         t.join()
+
+         print 'done'
+
+t = Downloader()
+t.start()
+
+time.sleep(1)
+
+t1 = Worker()
+t1.start()
+
+t2 = Worker()
+t2.start()
+
+t3 = Worker()
+t3.start()
