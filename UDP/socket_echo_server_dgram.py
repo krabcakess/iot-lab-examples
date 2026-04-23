9aebe91abe680dec60792b56d6503cfea5d33e02
Muhammad Izz Nabeel <s3986060@student.rmit.edu.au>
2026-04-23 10:41:41 +1000

    example(week7) ; upload code

---
+++ UDP/socket_echo_server_dgram.py
@@ -0,0 +1,38 @@
+import socket
+
+localIP     = "127.0.0.1"
+localPort   = 20001
+bufferSize  = 1024
+
+msgFromServer       = "Hello UDP Client"
+bytesToSend         = str.encode(msgFromServer)
+
+ 
+# Create a datagram socket
+# communication domain in which the socket should be created. Some of address families are AF_INET (IP), AF_INET6 (IPv6), 
+# AF_UNIX (local channel, similar to pipes), AF_ISO (ISO protocols), and AF_NS (Xerox Network Systems protocols).
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+while(True):
+
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP  = "Client IP Address:{}".format(address)
+    
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
+
+"""
+	Acknowledgement: Copyright of https://pythontic.com/modules/socket used
+	for educational learning only
+"""
